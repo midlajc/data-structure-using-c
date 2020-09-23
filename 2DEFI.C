9650a7e11c27799cb7a8ae7ec90c60fae4a7877d
Midlaj C <mail.midlajc@gmail.com>
2020-09-23 23:26:35 +0530

    added some programs

---
+++ 2DEFI.C
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+int array[100][100],m,n,i,sear_ele,find=1;
+printf("Enter base of matrix(M x N m&n limited to 100):");
+scanf("%d%d",&m,&n);
+for(i=0;i<m;i++)
+{
+int j;
+printf("\nenter elements to row %d(differentiate elements using space):",i+1);
+for(j=0;j<n;j++)
+{
+scanf("%d",&array[i][j]);
+}
+}
+printf("\nwhich element wanted to be searched:");
+scanf("%d",&sear_ele);
+for(i=0;i<m;i++)
+{
+ int j;
+ for(j=0;j<n;j++)
+ {
+  if(sear_ele==array[i][j])
+  {
+  find=0;
+  printf("\nElement founded at position %d x %d\n",i+1,j+1);
+  }
+ }
+}
+if(find==1)
+printf("\nelement not founded");
+getch();
+clrscr();
+}
--- STRREVUS.C
+++ STRREVUS.C
@@ -10,13 +10,12 @@ printf("insert a string(limit 100):");
 scanf("%[^\n]s",string);
 array_length=strlen(string);
 temp=string;
-//printf("%c\n",*temp);
 for(i=0,temp+=array_length-1;i<array_length;i++,temp--)
 {
 rev_string[i]=*temp;
 }
-rev_string[array_length+1]='\0';
-printf("\n%s",rev_string);
+rev_string[array_length]='\0';
+printf("\nreverse of enterd string: %s",rev_string);
 getch();
 clrscr();
 }
