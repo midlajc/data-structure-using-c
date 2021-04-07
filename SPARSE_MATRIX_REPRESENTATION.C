0421554f0ab391a0eed705965f5ed701e2d60bec
Midlaj C <mail.midlajc@gmail.com>
2021-04-07 11:12:00 +0530

    fixed sparse matrix issue

--- SPARSE_MATRIX_REPRESENTATION.C
+++ SPARSE_MATRIX_REPRESENTATION.C
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
+void main()
 {
 	int triplet[100][3], matrix[20][20];
-	int m, n, totalElements = 0, i, j, k;
-	printf("enter base of matrix(mxn):");
+	int m, n, totalElements = 0, i, j, k,zero_count=0;
+	clrscr() ;
+	printf("Enter order of matrix:");
 	scanf("%d%d", &m, &n);
 	printf("\n");
 	for (i = 0; i < m; i++)
@@ -13,13 +14,15 @@ int main()
 		for (j = 0; j < n; j++)
 		{
 			scanf("%d", &matrix[i][j]);
-			fflush(stdin);
+			if(matrix[i][j]==0)
+				zero_count++;
 		}
 	}
-
-	for (int i = 0; i < m; i++)
+	if(zero_count>(n*m/2))
 	{
-		for (int j = 0; j < n; j++)
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < n; j++)
 		{
 			if (matrix[i][j] != 0)
 			{
@@ -31,11 +34,14 @@ int main()
 		}
 	}
 	printf("\nTRIPLET REPRESENTATION\n\nrow\tcolumn\tvalue");
-	for (int i = 0; i < totalElements; i++)
+	for (i = 0; i < totalElements; i++)
 	{
 		printf("\n");
-		for (int j = 0; j < 3; j++)
+		for (j = 0; j < 3; j++)
 			(j < 2) ? printf("%d\t", triplet[i][j] + 1) : printf("%d\t", triplet[i][j]);
 	}
-	printf("\n");
+	}
+	else
+		printf("\nGiven Matrix is not a Sparse Matrix");
+	getch();
 }
